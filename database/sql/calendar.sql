b105fd14efeaad64bb8501679b2a884feb654ab4
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-09-14 11:58:59 -0300

    Adicionando script

---
+++ database/sql/calendar.sql
@@ -0,0 +1,10 @@
+create schema calendar default charset utf8;
+use calendar;
+create table event (
+	id int auto_increment primary key,
+    name varchar(100) not null,
+    start datetime not null unique,
+    end datetime not null unique
+    /* Aqui vai entrar o ID_User no lugar de nome */
+)engine = InnoDB;
+select * from event;
