81a0d92ea675d43e8c2fddbea723787e3441cda8
JuanvictorO <juanvictoroliveira2009@gmail.com>
2020-09-17 15:28:07 -0300

    Preparando views para o template

--- resources/views/nonOperatingDays.blade.php
+++ resources/views/nonOperatingDays.blade.php
@@ -1,134 +1,10 @@
-<!--**
- * @author Cesar Szpak - Celke - cesar@celke.com.br
- * @pagina desenvolvida usando FullCalendar e Bootstrap 4,
- * o código é aberto e o uso é free,
- * porém lembre-se de conceder os créditos ao desenvolvedor.
- *-->
-<!DOCTYPE html>
-<html>
+@extends('admin-template.dashboard')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="csrf-token" content="{{csrf_token ()}}">
+@section('content')
+<div id='calendar'></div>
+@endsection
 
-    <link href="{{ asset('/vendor/calendar/main.css') }}" rel='stylesheet'>
-
-    <link rel="stylesheet" href="{{ asset('vendor/bootstrap-4.4.1/bootstrap.min.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('/css/personalizado.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('/vendor/toastr/toastr.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/vendor/jquery-confirm/css/jquery-confirm.css') }}">
-
-    <script src="{{ asset('/vendor/calendar/main.js') }}"></script>
-    <script src="{{ asset('/vendor/calendar/locales/pt-br.js') }}"></script>
-
-    <script src="{{ asset('/vendor/jquery-3.4.1.min.js') }}"></script>
-    <script src="{{ asset('/vendor/bootstrap-4.4.1/popper.min.js') }}"></script>
-    <script src="{{ asset('/vendor/bootstrap-4.4.1/bootstrap.min.js') }}"></script>
-
-    <script src="{{ asset('/vendor/toastr/toastr.min.js') }}"></script>
-    <script src="{{ asset('/vendor/jquery-confirm/js/jquery-confirm.js') }}"></script>
-</head>
-
-<body>
-    <div id='calendar'></div>
-
-    <div class="modal fade" id="visualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Detalhes do Evento</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <dl class="row">
-                        <dt class="col-sm-3">ID do evento</dt>
-                        <dd class="col-sm-9" id="id"></dd>
-
-                        <dt class="col-sm-3">Título do evento</dt>
-                        <dd class="col-sm-9" id="title"></dd>
-
-                        <dt class="col-sm-3">Início do evento</dt>
-                        <dd class="col-sm-9" id="start"></dd>
-
-                        <dt class="col-sm-3">Fim do evento</dt>
-                        <dd class="col-sm-9" id="end"></dd>
-                    </dl>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Cadastrar Evento</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="form-event" method="post" action="{{ url('register') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Nome</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="name" class="form-control" id="Nome" placeholder="Nome" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Data</label>
-                            <div class="col-sm-10">
-                                <input type="date" name="start" class="form-control" id="start" readonly="readonly">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Horários Disponíveis</label>
-                            <div class="col-sm-10">
-                                <select name="time" class="form-control" id="time" required>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-sm-12 text-right">
-                                <input id="datetime" type="hidden" name="datetime">
-                                <input type="submit" value="Cadastrar" class="btn btn-success">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-<!--- MODAL SUCCESS OR ERROR --->
-<div class="modal fade" id="msg" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content py-4">
-            <div class="modal-body text-center">
-                <span id="span-msg"></span><br>
-                <span class="text-secondary">Clique fora da caixa para fechar</span>
-            </div>
-        </div>
-    </div>
-</div>
-<script>
-    toastr.options = {
-        "closeButton": true,
-        "preventDuplicates": true,
-        "progressBar": true,
-    }
-</script>
-
-<script>
-    const baseUrl = '<?= url('') ?>';
-</script>
+@push('script')
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
@@ -282,5 +158,4 @@
         });
     }
 </script>
-
-</html>
+@endpush
